e99e25d783078a547d91cc831b6d8049da670912
Minas <105938665+Minas02@users.noreply.github.com>
2024-11-13 18:49:46 +0100

    ajout de la fonction getNeighbours qui retourne le nombre de case vivantes autour et nextState qui calcule le prochaine etat de la case

--- src/modele/Case.java
+++ src/modele/Case.java
@@ -4,22 +4,71 @@ import java.util.Random;
 
 public class Case {
     private static final Random rnd = new Random();
+    private boolean state;
+    private int x;
+    private int y;
 
     public boolean getState() {
-
         return state;
     }
 
-    private boolean state;
+    public void getX() {
+        this.x = x;
+    }
+
+    public void getY() {
+        this.y = y;
+    }
 
     public void rndState() {
         state = rnd.nextBoolean();
     }
 
-    public void nextState() {
-        // TODO
-        // calcul de l'état suivant.
-        // Utiliser la fonction getCase de Environnement
+    private int getNeighbours(Environnement e) {
+        int n = 0
+        if (e.getCase(this, h)){
+            n++;
+        }
+        if (e.getCase(this, hd)){
+            n++;
+        }
+        if (e.getCase(this, d)){
+            n++;
+        }
+        if (e.getCase(this, db)){
+            n++;
+        }
+        if (e.getCase(this, b)){
+            n++;
+        }
+        if (e.getCase(this, bg)){
+            n++;
+        }
+        if (e.getCase(this, g)){
+            n++;
+        }
+        if (e.getCase(this, gh)){
+            n++;
+        }
+        retunr n;
+    }
+
+    public void nextState(Environnement e) {
+        if (state){
+            if (getNeighbours(e) < 2 || getNeighbours(e) > 3){
+                state = false;
+            }
+
+            else{
+                state = true;
+            }
+        }
+        else {
+            if (getNeighbours(e) = 3){
+                state = true;
+            }
+        }
+
     }
 
 
