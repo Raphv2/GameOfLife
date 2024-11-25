a47fcfbcbbae7936278b1e448dc549a57a7e956c
Minas <105938665+Minas02@users.noreply.github.com>
2024-11-25 16:46:02 +0100

    ajout des modules

---
+++ src/modele/Modules.java
@@ -0,0 +1,42 @@
+package modele;
+
+public class Modules{
+
+    public static int[][][] 3x3Blinker(){
+        int[][][] blinker;
+        for(int i=0; i<3; ++i){
+            blinker[i] = [i][0];
+        }
+        return blinker;
+    }
+
+    public static int[][][] 3x3SpaceShip(){
+        int[][][] spaceship;
+        int count = 0;
+        for(int i=0; i<3; ++i){
+            spaceship[i] = [i][0];
+            count++;
+        }
+        spaceship[count] = [2][1];
+        count++:
+        spaceship[count] = [1][2];
+
+        return spaceship;
+    }
+
+    public static int[][][] 5x4SpaceShip(){
+        int[][][] spaceship;
+        int count = 0;
+        for(int i=0; i<4; ++i){
+            spaceship[i] = [i+1][0]
+            count++;
+        }
+        spaceship[count] = [0][1];
+        spaceship[count] = [4][1];
+
+        spaceship[count] = [4][2];
+        
+        spaceship[count] = [0][3];
+        spaceship[count] = [3][3];
+    }
+}
