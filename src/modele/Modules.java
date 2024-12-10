e7b5c3f4c90305d70055f77ff0225ed5d5564d14
GOSSET RAPHAEL p2216446 <raphael.gosset@etu.univ-lyon1.fr>
2024-12-10 14:47:36 +0100

    class module corrigé

--- src/modele/Modules.java
+++ src/modele/Modules.java
@@ -1,43 +1,35 @@
 package modele;
 
-/*public class Modules{
+public class Modules {
 
-    public static int[][][] 3x3Blinker(){
-        int[][][] blinker;
-        for(int i=0; i<3; ++i){
-            blinker[i] = [i][0];
+    public static int[][] generate3x3Blinker(int offsetX, int offsetY) {
+        int[][] blinker = new int[3][2]; // Tableau de 3 cellules avec leurs coordonnées
+        for (int i = 0; i < 3; i++) {
+            blinker[i][0] = i + offsetX; // Ligne
+            blinker[i][1] = 1 + offsetY; // Colonne
         }
         return blinker;
     }
 
-    public static int[][][] 3x3SpaceShip(){
-        int[][][] spaceship;
-        int count = 0;
-        for(int i=0; i<3; ++i){
-            spaceship[i] = [i][0];
-            count++;
-        }
-        spaceship[count] = [2][1];
-        count++:
-        spaceship[count] = [1][2];
-
+    public static int[][] generate3x3SpaceShip(int offsetX, int offsetY) {
+        int[][] spaceship = new int[5][2]; // Tableau de 5 cellules avec leurs coordonnées
+        spaceship[0] = new int[]{0 + offsetX, 0 + offsetY};
+        spaceship[1] = new int[]{1 + offsetX, 0 + offsetY};
+        spaceship[2] = new int[]{2 + offsetX, 0 + offsetY};
+        spaceship[3] = new int[]{2 + offsetX, 1 + offsetY};
+        spaceship[4] = new int[]{1 + offsetX, 2 + offsetY};
         return spaceship;
     }
 
-    public static int[][][] 5x4SpaceShip(){
-        int[][][] spaceship;
-        int count = 0;
-        for(int i=0; i<4; ++i){
-            spaceship[i] = [i+1][0]
-            count++;
-        }
-        spaceship[count] = [0][1];
-        spaceship[count] = [4][1];
-
-        spaceship[count] = [4][2];
-        
-        spaceship[count] = [0][3];
-        spaceship[count] = [3][3];
+    public static int[][] generate5x4SpaceShip(int offsetX, int offsetY) {
+        int[][] spaceship = new int[7][2]; // Tableau de 7 cellules avec leurs coordonnées
+        spaceship[0] = new int[]{1 + offsetX, 0 + offsetY};
+        spaceship[1] = new int[]{2 + offsetX, 0 + offsetY};
+        spaceship[2] = new int[]{3 + offsetX, 0 + offsetY};
+        spaceship[3] = new int[]{4 + offsetX, 0 + offsetY};
+        spaceship[4] = new int[]{0 + offsetX, 1 + offsetY};
+        spaceship[5] = new int[]{4 + offsetX, 1 + offsetY};
+        spaceship[6] = new int[]{0 + offsetX, 3 + offsetY};
+        return spaceship;
     }
 }
-*/
